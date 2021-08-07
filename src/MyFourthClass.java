266876e115a32638aabc831e7d6508f1ac875764
techproed <abayram.techproed@gmail.com>
2021-08-07 13:56:04 -0500

    fouth class

---
+++ src/MyFourthClass.java
@@ -0,0 +1,10 @@
+public class MyFourthClass {
+
+    public static void main(String[] args) {
+        int x=543;
+        int sum = x+24;
+        System.out.println(sum);
+
+        int y=50;
+    }
+}
